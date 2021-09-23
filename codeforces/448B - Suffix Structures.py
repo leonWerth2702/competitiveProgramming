08555477c7c7abd0950dd09f07902bfbc1a312a2
leon_werth2702 <leon_werth2702@pop-os.localdomain>
2021-09-23 13:13:54 +0700

    codeforces

--- codeforces/448B - Suffix Structures.py
+++ codeforces/448B - Suffix Structures.py
@@ -11,16 +11,17 @@ def is_automaton(word1, word2):
 
 
 def is_array(word1, word2):
-    if len(word1) != len(word2):
-        return False
-    else:
+    if len(word1) == len(word2):
         list_word1 = sorted(list(word1))
         list_word2 = sorted(list(word2))
-
-        for index in range(len(list_word1)):
+        
+        for index in range(len(word1)):
             if list_word1[index] != list_word2[index]:
                 return False
-    return True
+            
+        return True
+    
+    return False
 
 
 def is_both(word1, word2):
