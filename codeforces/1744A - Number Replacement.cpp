79b8152f2888fef3f2b4ad17bb2e20c40f926597
leon_werth2702 <66047284+leonWerth2702@users.noreply.github.com>
2022-10-26 22:23:47 +0700

    Update 1744A - Number Replacement.cpp

--- codeforces/1744A - Number Replacement.cpp
+++ codeforces/1744A - Number Replacement.cpp
@@ -1,40 +1,32 @@
 #include <bits/stdc++.h>
-#define ll long long
+
 using namespace std;
- 
-void solve()
-{
+
+void solve() {
     int n;
     cin >> n;
     vector<int> arr(n);
     string s;
- 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; ++i) {
         cin >> arr[i];
+    }
     cin >> s;
- 
-    for (int i = 0; i < n - 1; i++)
-    {
-        for (int j = i + 1; j < n; j++)
-        {
-            if (arr[i] == arr[j] && s[i] != s[j])
-            {
-                cout << "NO\n";
+
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            if (arr[i] == arr[j] && s[i] != s[j]) {
+                cout << "NO" << endl;
                 return;
             }
         }
     }
-    cout << "YES\n";
+    cout << "YES" << endl;
 }
- 
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int t;
-    cin >> t;
-    while (t--)
-    {
+
+int main() {
+    int numberOfTestCase;
+    cin >> numberOfTestCase;
+    while (numberOfTestCase--) {
         solve();
     }
 }
