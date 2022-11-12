d534277df8d5d683582cff7f1402cb9e06b30f60
leon_werth2702 <66047284+leonWerth2702@users.noreply.github.com>
2022-11-12 19:28:13 +0700

    Create b8BSgC8y.cpp

---
+++ codeforces/b8BSgC8y.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    string commonPrefixUtil(string &str1, string &str2) {
+        string result;
+        int len = (int) min(str1.length(), str2.length());
+        for (int i = 0; i < len; ++i) {
+            if (str1[i] != str2[i]) {
+                break;
+            }
+            result += str1[i];
+        }
+        return result;
+    }
+    
+    string longestCommonPrefix(vector<string> &strs) {
+        if (strs.empty()) {
+            return "";
+        }
+        string ans = strs[0];
+        for (int i = 1; i < strs.size(); ++i) {
+            ans = commonPrefixUtil(ans, strs[i]);
+        }
+        return ans;
+    }
+};
